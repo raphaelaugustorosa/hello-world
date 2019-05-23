3d8be695785637a9dbca1c971ffda6f66cd712ce
Raphael Augusto da Silva Rosa <rosa_raphael_augusto@lilly.com>
2019-05-23 18:01:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pg = require('pg');
 var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
-
+//MY COMMENTS - RAPHA2
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
